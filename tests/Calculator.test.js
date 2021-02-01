2e19c06d17f355065b9fdb43a8785341edcfde0f
Derek <derekbradshaw01@gmail.com>
2021-02-01 14:40:11 -0500

    Test Merge Code

--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -4,7 +4,7 @@ const Calculator = require('../src/Calculator');
 test('Calculator Adding two numbers', () => {
 
     let calculation = Calculator.Sum(1, 2);
-
+    //expect is used to test if the results are correct
     expect(calculation.a).toBe(1);
     expect(calculation.b).toBe(2);
     expect(calculation.op).toBe("Sum");
