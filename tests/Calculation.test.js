2a0e6b7abeb3de074d5736ee097a2bc6caef17dc
Derek <derekbradshaw01@gmail.com>
2021-02-08 14:37:18 -0500

    Test Merge Code

--- tests/Calculation.test.js
+++ tests/Calculation.test.js
@@ -1,17 +1,46 @@
 const Calculation = require('../src/models/calculation');
 
+const Sum = require('../src/Operations/Sum');
+const Difference = require('../src/Operations/Difference');
+const Product = require('../src/Operations/Product');
+const Quotient = require('../src/Operations/Quotient');
 
 test('Calculate Object Creation', () => {
 
-    let calculation = new Calculation(1,2,"Sum");
+    let op = Sum;
+    let calculation = new Calculation(1,2,op);
     expect(calculation.a).toBe(1);
     expect(calculation.b).toBe(2);
-    expect(calculation.op).toBe("Sum");
+    expect(calculation.op).toBe(op);
 });
 
-test('Test Get Results function', () => {
+test('Test Get Results for Sum function', () => {
 
-    let calculation = new Calculation(1,2,"Sum");
+    let op = Sum;
+    let calculation = new Calculation(1,2,op);
     expect(calculation.GetResults()).toBe(3);
 });
+
+test('Test Get Results for Difference function', () => {
+
+    let op = Difference;
+    let calculation = new Calculation(2,1,op);
+    expect(calculation.GetResults()).toBe(1);
+});
+
+test('Test Get Results for Product function', () => {
+
+    let op = Product;
+    let calculation = new Calculation(2,3,op);
+    expect(calculation.GetResults()).toBe(6);
+});
+
+test('Test Get Results for Quotient function', () => {
+
+    let op = Quotient;
+    let calculation = new Calculation(10,2,op);
+    expect(calculation.GetResults()).toBe(5);
+});
+
+
 
--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -3,40 +3,45 @@ const Calculator = require('../src/Calculator');
 
 test('Calculator Adding two numbers', () => {
 
-    let calculation = Calculator.Sum(1, 2);
+    //let calculation = Calculator.Sum(1, 2);
     //expect is used to test if the results are correct
-    expect(calculation.a).toBe(1);
-    expect(calculation.b).toBe(2);
-    expect(calculation.op).toBe("Sum");
-    expect(calculation.GetResults()).toBe( 3);
+    //expect(calculation.a).toBe(1);
+    //expect(calculation.b).toBe(2);
+    //expect(calculation.op).toBe("Sum");
+    //expect(calculation.GetResults()).toBe( 3);
 });
 
+test('Calculator Adding two numbers', () => {
+
+    let result = Calculator.Sum(1, 2);
+    expect(result).toBe(3);
+});
 test('Calculator Subtracting two numbers', () => {
 
-    let calculation = Calculator.Difference(2, 1);
-    //expect is used to test if the results are correct
-    expect(calculation.a).toBe(2);
-    expect(calculation.b).toBe(1);
-    expect(calculation.op).toBe("Subtract");
-    expect(calculation.GetResults()).toBe( 1);
+    let result = Calculator.Difference(1, 2);
+    expect(result).toBe(-1);
+});
+test('Calculator Multiplying two numbers', () => {
+
+    let result = Calculator.Product(1, 2);
+    expect(result).toBe(2);
+});
+test('Calculator Dividing two numbers', () => {
+
+    let result = Calculator.Quotient(1, 2);
+    expect(result).toBe(.5);
 });
 
-test('Calculator Multiply Two Number', () => {
+test('Calculator adding to Calculations[]', () => {
 
-    let calculation = Calculator.Multiply(2, 3);
-    //expect is used to test if the results are correct
-    expect(calculation.a).toBe(2);
-    expect(calculation.b).toBe(3);
-    expect(calculation.op).toBe("Multiply");
-    expect(calculation.GetResults()).toBe( 6);
+    let result = Calculator.Quotient(1, 2);
+    let calculations = Calculator.Calculations
+   calculations.forEach(function (calculation){
+        console.log(calculation);
+
+    });
+    expect(result).toBe(.5);
 });
 
-test('Calculator Divide Two Number', () => {
 
-    let calculation = Calculator.Divide(10, 2);
-    //expect is used to test if the results are correct
-    expect(calculation.a).toBe(10);
-    expect(calculation.b).toBe(2);
-    expect(calculation.op).toBe("Divide");
-    expect(calculation.GetResults()).toBe( 5);
-});
+
